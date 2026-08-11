936578d363440b9aa9c389b404889a81dfc5f7b9
ducthang140 <doducthang1604@gmail.com>
2026-08-11 14:16:52 +0700

    added control buttons

--- Assets/Script/LRControl.cs
+++ Assets/Script/LRControl.cs
@@ -8,6 +8,10 @@ public class LRControl : MonoBehaviour
     public Logic LogicPass;
     public bool SSS = true;
 
+    // Mobile button states
+    private bool leftPressed = false;
+    private bool rightPressed = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,18 +21,26 @@ public class LRControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool leftPressed = Keyboard.current.leftArrowKey.isPressed;
-        bool rightPressed = Keyboard.current.rightArrowKey.isPressed;
+        // Keyboard input
+        bool keyboardLeft = Keyboard.current != null &&
+                            Keyboard.current.leftArrowKey.isPressed;
 
-        if (leftPressed && rightPressed)
+        bool keyboardRight = Keyboard.current != null &&
+                             Keyboard.current.rightArrowKey.isPressed;
+
+        // Combine keyboard and mobile input
+        bool moveLeft = keyboardLeft || leftPressed;
+        bool moveRight = keyboardRight || rightPressed;
+
+        if (moveLeft && moveRight)
         {
             MyRigidBody.linearVelocity = Vector2.zero;
         }
-        else if (leftPressed && SSS)
+        else if (moveLeft && SSS)
         {
             MyRigidBody.linearVelocity = Vector2.left * ControlStrength;
         }
-        else if (rightPressed && SSS)
+        else if (moveRight && SSS)
         {
             MyRigidBody.linearVelocity = Vector2.right * ControlStrength;
         }
@@ -38,9 +50,33 @@ public class LRControl : MonoBehaviour
         }
     }
 
+    // LEFT BUTTON
+
+    public void LeftButtonDown()
+    {
+        leftPressed = true;
+    }
+
+    public void LeftButtonUp()
+    {
+        leftPressed = false;
+    }
+
+    // RIGHT BUTTON
+
+    public void RightButtonDown()
+    {
+        rightPressed = true;
+    }
+
+    public void RightButtonUp()
+    {
+        rightPressed = false;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Obstacle"))
+        if (collision.gameObject.CompareTag("Obstacle"))
         {
             LogicPass.GameOver();
             SSS = false;
